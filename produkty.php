3fea0a84017b8a49a6ebeed1c7b22bbeb08346cc
TomasZsembera <tzsemberaa@gmail.com>
2024-04-09 18:03:46 +0200

    produkty_podstranka

---
+++ produkty.php
@@ -0,0 +1,22 @@
+<?php
+include_once 'header.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Doplnenie metadata-->
+    <meta name="description" content="Main">
+    <meta name="keywords" content="Domov">
+    <meta name="author" content="Tomáš Zsembera">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/slider.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+<?php include_once 'footer.php'; ?>
